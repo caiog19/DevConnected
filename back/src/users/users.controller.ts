296b93c974811a261a6eec88d00587b2c3ce7cbc
Caio Gullo <caioinacio19@gmail.com>
2024-11-30 23:09:47 -0300

    feat: Add user controller

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User, Prisma } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() data: Prisma.UserCreateInput): Promise<User> {
+    return this.usersService.createUser(data);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User | null> {
+    return this.usersService.findOne(Number(id));
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: Prisma.UserUpdateInput,
+  ): Promise<User> {
+    return this.usersService.updateUser(Number(id), data);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<User> {
+    return this.usersService.deleteUser(Number(id));
+  }
+}
