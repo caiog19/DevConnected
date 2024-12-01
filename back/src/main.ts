ec0c055836568ce47114cd1155c766c803392841
Caio Gullo <caioinacio19@gmail.com>
2024-12-01 11:43:25 -0300

    fix: Cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+  
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    allowedHeaders: 'Content-Type, Accept, Authorization', 
+  });
+
+  await app.listen(5000); 
 }
+
 bootstrap();
