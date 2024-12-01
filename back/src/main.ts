7fe147c16c594d43d68a2bab320946f885a4fc81
Caio Gullo <caioinacio19@gmail.com>
2024-12-01 20:50:25 -0300

    fix: Cors para deploy e desenvolvimento

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,14 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+
   app.enableCors({
-    origin: 'https://dev-connected.vercel.app/', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
-    allowedHeaders: 'Content-Type, Accept, Authorization', 
+    origin: [
+      'http://localhost:3000',  
+      'https://dev-connected.vercel.app',  
+      'https://devconnected.onrender.com', 
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  await app.listen(5000); 
+  await app.listen(5000);  
 }
 
 bootstrap();
