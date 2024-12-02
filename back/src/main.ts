a8719b41dbbead9ba9acfbde5dd7860cf737dea5
Caio Gullo <caioinacio19@gmail.com>
2024-12-01 21:33:24 -0300

    cleanup: limpando main.ts

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaClient } from '@prisma/client';
-import { exec } from 'child_process';
-import { promisify } from 'util';
-
-const execAsync = promisify(exec);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,16 +14,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  // Aplicar migrações do Prisma automaticamente
-  try {
-    console.log('Aplicando migrações...');
-    await execAsync('npx prisma migrate deploy');
-    console.log('Migrações aplicadas com sucesso!');
-  } catch (error) {
-    console.error('Erro ao aplicar migrações:', error);
-  }
-
-  // Iniciar o servidor
   await app.listen(process.env.PORT || 5000);
 }
 
